25d768190ff1f2bb27ff42628cfc6a4a5708e242
Artyom Aralov <artyom.aralov@gmail.com>
2020-02-05 18:25:52 +0200

    AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import {HomeComponent} from './home/home.component';
 import {ProductsComponent} from './products/products.component';
 import {NewProductComponent} from './new-product/new-product.component';
 import {EditProductComponent} from './edit-product/edit-product.component';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register-success', component: RegisterSuccessComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'add-product', component: NewProductComponent},
-  {path: 'edit-product/:id', component: EditProductComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'products', component: ProductsComponent, canActivate: [AuthGuard]},
+  {path: 'add-product', component: NewProductComponent, canActivate: [AuthGuard]},
+  {path: 'edit-product/:id', component: EditProductComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
